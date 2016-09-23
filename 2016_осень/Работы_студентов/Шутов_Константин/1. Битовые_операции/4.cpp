4be47c8ee6b7955ccd298e6fe136a953baa378a0
Constantine <kostetmilic7@gmail.com>
2016-09-24 01:21:42 +1000

    Èñïðàâëåíû áàãè

--- 2016_осень/Работы_студентов/Шутов_Константин/1. Битовые_операции/4.cpp
+++ 2016_осень/Работы_студентов/Шутов_Константин/1. Битовые_операции/4.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
-int set1(int N, int P) 
+int set1(char N, int P) 
 {
     return (N | (1 << P));
 }
 
-int set0(int N, int P) 
+int set0(char N, int P) 
 {
     return (N & ~(1 << P));
 }
 
-bool bit(int N, int P) 
+bool bit(char N, int P) 
 {
     return (N & ( 1 << P));
 }
@@ -20,28 +20,25 @@ char s,p=0,c=0;
 
 int main()
 {
-	scanf("%s",&s);
-	if(s=='q') { return 0;}
-	else if(s=='s')
+	for(;;)
 	{
-		printf("Vvedite poziciu ot 1 do 8\n");
-		scanf("%d",&p);
-		printf("Vvedite sostoyanie bita (1 - true, 2 - false)\n");
-		scanf("%d",&c);
-		if(c==1) {n=set1(n,p-1); printf("Bit ustanovlen na 1\n");}
-		else if(c==2) {n=set0(n,p-1); printf("Bit ustanovlen na 0\n");}
-		return main();
-	}
-	else if(s=='g')
-	{
-		printf("Vvedite poziciu ot 1 do 8\n");
-		scanf("%d",&p);
-		printf("Sostoyanie %d-ogo bita = %d\n",p,bit(n,p-1));
-		return main();
-	}
-	else 
-	{
-		printf("Nevernaya komanda\n");		
-		return main();
+		scanf("%s",&s);
+		if(s=='q') return 0;
+		else if(s=='s')
+		{
+			printf("Vvedite poziciu ot 1 do 8\n");
+			scanf("%d",&p);
+			printf("Vvedite sostoyanie bita (1 - true, 2 - false)\n");
+			scanf("%d",&c);
+			if(c==1) {n=set1(n,p-1); printf("Bit ustanovlen na 1\n");}
+			else if(c==2) {n=set0(n,p-1); printf("Bit ustanovlen na 0\n");}
+		}
+		else if(s=='g')
+		{
+			printf("Vvedite poziciu ot 1 do 8\n");
+			scanf("%d",&p);
+			printf("Sostoyanie %d-ogo bita = %d\n",p,bit(n,p-1));
+		}
+		else printf("Nevernaya komanda\n");				
 	}
 }
