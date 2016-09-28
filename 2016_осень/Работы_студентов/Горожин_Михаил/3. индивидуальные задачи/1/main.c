4d7e7dad7848644730663456dbbc1eb9d7e5d5aa
Mikhail Gorozhin <m.gorozhin@gmail.com>
2016-09-28 23:39:43 +1000

    improved performance

--- 2016_осень/Работы_студентов/Горожин_Михаил/3. индивидуальные задачи/1/main.c
+++ 2016_осень/Работы_студентов/Горожин_Михаил/3. индивидуальные задачи/1/main.c
@@ -3,31 +3,47 @@
 #define N 10
 
 int main(){
-  int arr[N] = {1, 1, 2, 3, 5, 8, 13, 21, 44, 65};
-  //int arr[N] = {0};
+  int arr[N] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+  //int arr[N] = {};
+
   int sumToFind;
   printf("Input number to find: ");
   scanf("%d", &sumToFind);
 
-  int c = 0;
-  for(int i = 0, j = 1; (i < N) && (j < N);){
-    // printf("i: %d, j: %d\n", i, j);
-    if ((arr[i] + arr[j]) == sumToFind){
-      // print them all
-      printf("arr[%d] + arr[%d] = \n"
-	     "= %d + %d =\n"
-	     "= %d\n\n",
-	     i, j, arr[i], arr[j], arr[i]+arr[j]);
-      c++;
-      // or not all
-      // break;
+  int li = 0;
+  for (int l = li+1, r = N;;) {
+    // when we finished with one index switch to the next one 
+    if ((r <= l)){
+      li++;
+      l = li+1;
+      r = N;
     }
-    j++;
-    if (j == N){
-      j = ++i + 1; // no need to check reversed pairs i.e. (1 + 2) == (2 + 1)
-      // and pairs where i < j, because they were already checked
+
+    // exit condition
+    // we exit, if leftmost element in the
+    // array is bigger then a sum, we look for,
+    // or if we looked throught all the indexes
+    if ((li >= N) || (arr[li] > sumToFind)){
+      printf("Pair, that sums to %d not found\n", sumToFind);
+      break;
+    } 
+    
+    
+    // midpoint
+    int m = l + (r - l) / 2;
+
+    // we find the according one with binary search
+    if ((sumToFind - arr[li]) == arr[m]) {
+      printf("arr[%d] + arr[%d] = \n"
+	     "%d + %d = \n"
+	     "= %d\n",
+	     li, m, arr[li], arr[m], sumToFind);
+      break;
+    } else if ((sumToFind - arr[li]) < arr[m]) {
+      r = m;
+    } else {
+      l = m + 1;
     }
   }
-  if (!c)
-    printf("Pair, that sums to %d not found\n", sumToFind);
+
 }
