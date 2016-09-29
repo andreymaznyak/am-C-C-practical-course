1aa23066431535719016341cc9f9eadbcb4b7188
Mikhail Gorozhin <m.gorozhin@gmail.com>
2016-09-29 19:53:25 +1000

    rev 3

--- 2016_осень/Работы_студентов/Горожин_Михаил/3. индивидуальные задачи/1/main.c
+++ 2016_осень/Работы_студентов/Горожин_Михаил/3. индивидуальные задачи/1/main.c
@@ -7,43 +7,31 @@ int main(){
   //int arr[N] = {};
 
   int sumToFind;
+
   printf("Input number to find: ");
   scanf("%d", &sumToFind);
 
-  int li = 0;
-  for (int l = li+1, r = N;;) {
-    // when we finished with one index switch to the next one 
-    if ((r <= l)){
-      li++;
-      l = li+1;
-      r = N;
+  for (int l = 0, r = N-1; l < r;) {
+    int s = arr[l] + arr[r];
+
+    if (s == sumToFind) {
+      // found
+      printf("arr[%d] + arr[%d] =\n"
+	     "= %d + %d =\n"
+	     "= %d\n\n", l, r, arr[l], arr[r], sumToFind);
+      return 0;
     }
-
-    // exit condition
-    // we exit, if leftmost element in the
-    // array is bigger then a sum, we look for,
-    // or if we looked throught all the indexes
-    if ((li >= N) || (arr[li] > sumToFind)){
-      printf("Pair, that sums to %d not found\n", sumToFind);
-      break;
-    } 
-    
-    
-    // midpoint
-    int m = l + (r - l) / 2;
-
-    // we find the according one with binary search
-    if ((sumToFind - arr[li]) == arr[m]) {
-      printf("arr[%d] + arr[%d] = \n"
-	     "%d + %d = \n"
-	     "= %d\n",
-	     li, m, arr[li], arr[m], sumToFind);
-      break;
-    } else if ((sumToFind - arr[li]) < arr[m]) {
-      r = m;
-    } else {
-      l = m + 1;
+    else {
+      if (s < sumToFind) {
+	// not enought
+	l++;
+      } else{
+	// too many
+	r--;
+      }
     }
   }
-
+  
+  printf("Pair, that sums to %d not found\n", sumToFind);
+  return 0;
 }
