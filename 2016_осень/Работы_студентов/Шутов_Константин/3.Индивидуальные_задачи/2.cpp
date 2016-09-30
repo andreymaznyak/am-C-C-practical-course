318da1076e32134e653392a3413ce0de634c3e11
N0N1N <kostetmilic7@gmail.com>
2016-09-30 21:15:36 +1000

    Update 2.cpp

--- 2016_осень/Работы_студентов/Шутов_Константин/3.Индивидуальные_задачи/2.cpp
+++ 2016_осень/Работы_студентов/Шутов_Константин/3.Индивидуальные_задачи/2.cpp
@@ -2,18 +2,18 @@
 #define N 10
 int main()
 {
-	int a[N]={1,2,3,4,5,6,7,8,9,10}, c;
+	int a[N]={1,2,3,4,5,6,7,8,9,10}, c, v=0;
 	scanf("%d",&c);
 	for(int i=0, j=N-1; i!=j;)
 	{
 		if(a[i] + a[j] == c) 
 		{
+			v++;
 			printf("%d[%d], %d[%d]\n", a[i], i, a[j], j);
-			return 0;
 		}
 		j--;
 		if(a[i] + a[j] < c) i++, j=N-1;	
 	}
-	printf("Òàêèå ÷èñëà íå íàøëè\n");
+	if(v==0) printf("Ã’Ã ÃªÃ¨Ã¥ Ã·Ã¨Ã±Ã«Ã  Ã­Ã¥ Ã­Ã Ã¸Ã«Ã¨\n");
 	return 0;
 }
