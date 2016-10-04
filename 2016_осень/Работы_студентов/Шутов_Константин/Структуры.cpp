2d0b85cccbf5a3385041b5df128ddf560b213373
Constantine <kostetmilic7@gmail.com>
2016-10-04 21:47:13 +1000

    Добавил решение задачи

---
+++ 2016_осень/Работы_студентов/Шутов_Константин/Структуры.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#define N 5
+using namespace std;
+
+class STUDENT
+{
+	private:
+	string *name;
+	int *number;
+	int *usp;
+	
+	public:
+	static bool t;
+	
+	bool Check (int *a)
+	{
+		int i;
+		for(i=0; i<N; i++) if(a[i]==2)  {t=true; return true; }
+		return false;
+	}
+	
+	STUDENT()
+	{
+		name = new string;
+		number = new int;
+		usp = new int[N];	
+		cin >> *name;
+		cin >> *number;
+		for(int i=0; i<N; i++) cin >> usp[i];
+	}
+	
+	~STUDENT()
+	{
+		if(Check(usp)) cout<<*name<<" "<<*number<<endl;
+		delete name;
+		delete number;
+		delete [] usp;
+	}
+};
+
+bool STUDENT::t=false;
+
+int main()
+{
+	STUDENT *spisok = new STUDENT[2];
+	if(!STUDENT::t) cout<<"Takih studentov net"<<endl;
+	delete [] spisok;
+	return 0;
+}
