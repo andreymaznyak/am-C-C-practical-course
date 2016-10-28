48e5016f895cc5a217b427bbb653df86e73df761
HaidenTenno <haiden_tenno@mail.ru>
2016-10-28 11:22:49 +1000

    Добавил задачу на cout

---
+++ 2016_осень/Работы_студентов/Тартынских_Петр/Занятие 5/Logo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+void printLogo() {
+	cout << "$$  $$   $$$$   $$$$$$  $$$$$   $$$$$   $$  $$" << endl;
+	cout << "$$  $$  $$  $$    $$    $$  $$  $$      $$$ $$" << endl;
+	cout << "$$$$$$  $$$$$$    $$    $$  $$  $$$$    $$ $$$" << endl;
+	cout << "$$  $$  $$  $$    $$    $$  $$  $$      $$  $$" << endl;
+	cout << "$$  $$  $$  $$  $$$$$$  $$$$$   $$$$$   $$  $$" << endl;
+	cout << "                                 PETR TARTYNSKIKH(c)" << endl;
+}
+
+int main (){
+	printLogo();
+	return 0;
+}
