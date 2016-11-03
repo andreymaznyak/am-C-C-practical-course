dc6e05b73af47e30c750e6130f5ab02d9b84df33
Timofey21 <timofey.milov41@gmail.com>
2016-11-03 21:34:23 +1000

    tasks of the 1 week

---
+++ 2016_осень/Работы_студентов/Милов_Тимофей/Битовые_операции/2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+			int a=0;
+			int b=0;
+			int i=0;
+			int f=1;
+			printf("Введите 2 числа\n");
+					scanf("%d", &a);
+					scanf("%d", &b);
+			if (a^b) printf("Числа не равны\n");
+			else printf("Числа равны\n");
+			
+
+}
